3c07243785d2ef32cd440983072751efd62fedaa
povloid <t34box@gmail.com>
2012-02-29 00:49:00 +0600

    Создан первый пробный рабочий тест

---
+++ busterminal/src/test/java/pk/home/busterminal/tests/TestFirst.java
@@ -0,0 +1,60 @@
+package pk.home.busterminal.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class,
+		TransactionalTestExecutionListener.class })
+@Transactional
+@ContextConfiguration(locations = { "file:./src/main/resources/applicationContext.xml" })
+public class TestFirst {
+	
+	/**
+	 * The Spring application context.
+	 * 
+	 */
+	@SuppressWarnings("unused")
+	private ApplicationContext context;
+	
+	
+	/**
+	 * Instantiates a new TestrbServiceTest.
+	 * 
+	 */
+	public TestFirst() {
+		setupRequestContext();
+	}
+
+	/**
+	 * Sets Up the Request context
+	 * 
+	 */
+	private void setupRequestContext() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		ServletRequestAttributes attributes = new ServletRequestAttributes(
+				request);
+		RequestContextHolder.setRequestAttributes(attributes);
+	}
+
+	
+	
+	@Test
+	public void test0()  throws Exception{
+		System.out.println("Run test 0");
+	}
+	
+}
