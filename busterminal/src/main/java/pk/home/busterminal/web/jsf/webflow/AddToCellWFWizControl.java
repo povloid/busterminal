215014f516a096a9a67101217654318280f1c732
povloid <t34box@gmail.com>
2012-03-14 15:39:31 +0600

    Промежуточный коммит

--- busterminal/src/main/java/pk/home/busterminal/web/jsf/webflow/AddToCellWFWizControl.java
+++ busterminal/src/main/java/pk/home/busterminal/web/jsf/webflow/AddToCellWFWizControl.java
@@ -10,9 +10,22 @@ public class AddToCellWFWizControl extends AWFWizart implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1586242585286422108L;
+
 	
-	
-	
-	
+	@Override
+	protected void nextImpl() throws Exception {
+		if(select == 0)
+			throw new Exception("Выберите вариант!");
+	}
+
+	private short select;
+
+	public short getSelect() {
+		return select;
+	}
+
+	public void setSelect(short select) {
+		this.select = select;
+	}
 
 }
