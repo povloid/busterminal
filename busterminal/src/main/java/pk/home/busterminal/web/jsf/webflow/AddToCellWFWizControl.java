9bc4bd48a688d967e9f902389f163cdcff3a8b84
povloid <t34box@gmail.com>
2012-03-14 14:51:59 +0600

    промежуточный коммит

---
+++ busterminal/src/main/java/pk/home/busterminal/web/jsf/webflow/AddToCellWFWizControl.java
@@ -0,0 +1,18 @@
+package pk.home.busterminal.web.jsf.webflow;
+
+import java.io.Serializable;
+
+import pk.home.libs.combine.web.jsf.flow.AWFWizart;
+
+public class AddToCellWFWizControl extends AWFWizart implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1586242585286422108L;
+	
+	
+	
+	
+
+}
