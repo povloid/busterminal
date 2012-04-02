fe44bbded50aa1a9e31b7c239cb8f3ff435e35ac
povloid <t34box@gmail.com>
2012-04-02 17:35:32 +0600

    Промежуточный коммит

--- busterminal/src/main/java/pk/home/busterminal/domain/Race.java
+++ busterminal/src/main/java/pk/home/busterminal/domain/Race.java
@@ -3,51 +3,67 @@ package pk.home.busterminal.domain;
 import java.io.Serializable;
 import java.lang.Long;
 import java.lang.String;
+import java.util.Date;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 /**
- * Entity class: Race
- * Race - рейс
- *
+ * Entity class: Race Race - рейс
+ * 
  */
 @Entity
 @Table(schema = "public", name = "Race")
 @NamedQueries({
-	@NamedQuery(name = "Race.findAll", query = "select a from Race a order by a.id"),
-	@NamedQuery(name = "Race.findByPrimaryKey", query = "select a from Race a where a.id = ?1")})
+		@NamedQuery(name = "Race.findAll", query = "select a from Race a order by a.id"),
+		@NamedQuery(name = "Race.findByPrimaryKey", query = "select a from Race a where a.id = ?1") })
 public class Race implements Serializable {
 
-	   
-	
+	@Version
+	private int version;
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Column(nullable = false)
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotNull
-    @Column(unique=true, nullable = false)
+	@Column(unique = true, nullable = false)
 	private String keyName;
-	
-	
+
 	private String description;
-	
+
+	// -------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(unique = true, nullable = false)
+	private Date dTime;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private BusRoute busRoute;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Bus bus;
 
 	public Race() {
 		super();
-	}   
+	}
+
 	public Long getId() {
 		return this.id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
-	}   
+	}
+
 	public String getKeyName() {
 		return this.keyName;
 	}
@@ -55,7 +71,8 @@ public class Race implements Serializable {
 	public void setKeyName(String keyName) {
 		System.out.println(keyName);
 		this.keyName = keyName;
-	}   
+	}
+
 	public String getDescription() {
 		return this.description;
 	}
@@ -63,7 +80,39 @@ public class Race implements Serializable {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public Date getdTime() {
+		return dTime;
+	}
+
+	public void setdTime(Date dTime) {
+		this.dTime = dTime;
+	}
+
+	public BusRoute getBusRoute() {
+		return busRoute;
+	}
+
+	public void setBusRoute(BusRoute busRoute) {
+		this.busRoute = busRoute;
+	}
+
+	public Bus getBus() {
+		return bus;
+	}
+
+	public void setBus(Bus bus) {
+		this.bus = bus;
+	}
+
 	@Override
 	public int hashCode() {
 		int hash = 0;
@@ -89,5 +138,5 @@ public class Race implements Serializable {
 	public String toString() {
 		return "pk.home.busterminal.domain.Race[ id=" + id + " ]";
 	}
-   
+
 }
