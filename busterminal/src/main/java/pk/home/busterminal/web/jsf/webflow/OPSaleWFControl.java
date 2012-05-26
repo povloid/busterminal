40b70332a994542cf58c6c6abc89e4ba7f40ef9f
povloid <povloid@proton2>
2012-05-26 16:27:43 +0600

    Промежуточный коммит

--- busterminal/src/main/java/pk/home/busterminal/web/jsf/webflow/OPSaleWFControl.java
+++ busterminal/src/main/java/pk/home/busterminal/web/jsf/webflow/OPSaleWFControl.java
@@ -1,8 +1,13 @@
 package pk.home.busterminal.web.jsf.webflow;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import pk.home.busterminal.domain.Race;
+import pk.home.busterminal.domain.Schema;
+import pk.home.busterminal.domain.Seat;
+import pk.home.busterminal.service.BusTempliteMasterService;
 import pk.home.busterminal.service.RaceService;
 import pk.home.libs.combine.web.jsf.flow.AWFBasicControl;
 
@@ -19,11 +24,18 @@ public class OPSaleWFControl extends AWFBasicControl implements Serializable {
 	 */
 	private static final long serialVersionUID = -6702785294921753935L;
 
+	private Race race;
+
+	// SERVICES
+	// -------------------------------------------------------------------------------------------
+
 	public RaceService getRaceService() {
 		return (RaceService) findBean("raceService");
 	}
 
-	private Race race;
+	public BusTempliteMasterService service() {
+		return (BusTempliteMasterService) findBean("busTempliteMasterService");
+	}
 
 	/**
 	 * Поиск рейса
@@ -33,6 +45,9 @@ public class OPSaleWFControl extends AWFBasicControl implements Serializable {
 	 */
 	public void findRace(long id) throws Exception {
 		this.race = getRaceService().find(id);
+		race.setBus(service().getBusService().findWithLazy(
+				race.getBus().getId()));
+
 		System.out.println("RACE " + id + ": " + race);
 	}
 
@@ -42,7 +57,99 @@ public class OPSaleWFControl extends AWFBasicControl implements Serializable {
 	@Override
 	protected void init0() throws Exception {
 		System.out.println("INIT");
+		handleSchemaChange();
 	}
+
+	/**
+	 * Продать место
+	 * @return
+	 */
+	public String sale() {
+		return "sale";
+	}
+	
+	
+	// Schemas
+	// -------------------------------------------------------------------------------------------
+
+	private Long selectedSchemeId;
+	private Schema selectedScheme;
+
+	/**
+	 * Выбор схемы
+	 * 
+	 * @throws Exception
+	 */
+	public void handleSchemaChange() throws Exception {
+		if (selectedSchemeId != null) {
+
+			boolean selected = false;
+			for (Schema s : race.getBus().getSchemas()) {
+				if (s.getId().equals(selectedSchemeId)) {
+					selectedScheme = s;
+					selected = true;
+					break;
+				}
+			}
+
+			if (!selected) {
+				selectedScheme = null;
+				selectedSchemeId = null;
+				race.setBus(service().getBusService().findWithLazy(
+						race.getBus().getId()));
+			}
+		}
+	}
+
+	/**
+	 * Формирование коллекций для отрисовки схемы
+	 * 
+	 * @return
+	 */
+	public List<List<Cell>> getCells() {
+
+		List<List<Cell>> ylist = new ArrayList<List<Cell>>();
+
+		try {
+			if (selectedScheme != null) {
+
+				selectedScheme = service().getSchemaService().findAllLazy(
+						selectedScheme.getId());
+
+				Cell pCell = null;
+				for (short y = 1; y < selectedScheme.getySize() + 1; ++y) {
+					List<Cell> xList = new ArrayList<Cell>();
+					for (short x = 1; x < selectedScheme.getxSize() + 1; ++x) {
+
+						Seat seat = null;
+						for (Seat s : selectedScheme.getSeats()) {
+							if (s.getSx() != null && s.getSy() != null
+									&& s.getSx() == x && s.getSy() == y) {
+								seat = s;
+								break;
+							}
+						}
+
+						Cell cell = new Cell(pCell, seat, x, y);
+						xList.add(cell);
+						// ----------
+						pCell = cell;
+					}
+					// --------------
+					ylist.add(xList);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ylist;
+	}
+
+	// CELL OPERATIONS
+	// ---------------------------------------------------------------------------------
+	private Cell selectedCell;
 
 	// get's and set's
 	// -------------------------------------------------------------------------------------------
@@ -53,6 +160,30 @@ public class OPSaleWFControl extends AWFBasicControl implements Serializable {
 
 	public void setRace(Race race) {
 		this.race = race;
+	}
+
+	public Long getSelectedSchemeId() {
+		return selectedSchemeId;
+	}
+
+	public void setSelectedSchemeId(Long selectedSchemeId) {
+		this.selectedSchemeId = selectedSchemeId;
+	}
+
+	public Schema getSelectedScheme() {
+		return selectedScheme;
+	}
+
+	public void setSelectedScheme(Schema selectedScheme) {
+		this.selectedScheme = selectedScheme;
+	}
+
+	public Cell getSelectedCell() {
+		return selectedCell;
+	}
+
+	public void setSelectedCell(Cell selectedCell) {
+		this.selectedCell = selectedCell;
 	}
 
 }
