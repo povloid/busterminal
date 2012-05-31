6a901c6426f7b8fda95f27c4c6e7d29a1cc02468
povloid <povloid@proton-m>
2012-05-31 21:34:54 +0600

    Переработка механизма отчетов, теперь забирает картинку через сервлет mvc + внутри ждет пока появится картинка

---
+++ busterminal/src/main/java/pk/home/busterminal/application/Config.java
@@ -0,0 +1,15 @@
+package pk.home.busterminal.application;
+
+
+/**
+ * Конфигурационный фаил
+ * @author povloid
+ *
+ */
+public final class Config {
+
+	public static final int BUFFER_SIZE = 512;
+
+	public static final String BASE_FILES_DIR = "/opt/vta-data/";
+
+}
--- busterminal/src/main/java/pk/home/busterminal/web/mvc/ReportsMVCController.java
+++ busterminal/src/main/java/pk/home/busterminal/web/mvc/ReportsMVCController.java
@@ -1,6 +1,9 @@
 package pk.home.busterminal.web.mvc;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -9,10 +12,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperReport;
@@ -27,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import pk.home.busterminal.application.Config;
 import pk.home.busterminal.domain.Order;
 import pk.home.busterminal.service.OrderService;
 import pk.home.libs.combine.fileutils.FileUtils;
@@ -99,6 +106,103 @@ public final class ReportsMVCController {
 	// простой вариант
 	// -----------------------------------------------------------------------------------------------------------------
 
+	@RequestMapping(value = "/images/{yyyy:.*}/{mm:.*}/{dd:.*}/{htmlImageUniqueSuffix:.*}/{file:.*}", method = RequestMethod.GET)
+	protected void image(@PathVariable String file, @PathVariable String yyyy,
+			@PathVariable String mm, @PathVariable String dd,
+			@PathVariable String htmlImageUniqueSuffix,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		// get the filename from the "file" parameter
+
+		if (file == null || file.equals("")) {
+			throw new ServletException(
+					"Invalid or non-existent file parameter in Image servlet.");
+		}
+
+		// Устранение уязвимости
+		file = file.replace("../", "");
+
+		// add the .pdf suffix if it doesn't already exist
+
+		BufferedInputStream buf = null;
+		FileInputStream in = null;
+		OutputStream out = null;
+
+		try {
+
+			out = response.getOutputStream();
+			File pdf = new File(Config.BASE_FILES_DIR + "/" + yyyy + "/" + mm
+					+ "/" + dd + "/" + htmlImageUniqueSuffix + "/" + file);
+
+			int index = 0;
+			while (!pdf.exists()) {
+				Thread.sleep(1000);
+				System.out.println(index++);
+				if (index == 50) {
+					break;
+				}
+			}
+
+			// set response headers
+
+			// Get the absolute path of the image
+			// /ServletContext sc = request.getSession().getServletContext();
+			// //String filename = sc.getRealPath("" + pdf);
+
+			// ////////System.out.println(filename);
+
+			// Get the MIME type of the image
+			// String mimeType = sc.getMimeType(pdf.getAbsolutePath());
+			String mimeType = "image/gif";
+
+			// ////////System.out.println(mimeType);
+
+			// ////////sc.log("MIME TYPE: " + mimeType);
+
+			// /if (mimeType == null) {
+			// / sc.log("Could not get MIME type of " + pdf.getAbsolutePath());
+			// /
+			// response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			// / return;
+			// /}
+
+			// Set content type
+			response.setContentType(mimeType);
+
+			// Это раскоментировать если сделать как атачмент
+			// response.addHeader("Content-Disposition", "attachment; filename="
+			// + fileName);
+
+			response.setContentLength((int) pdf.length());
+
+			FileInputStream input = new FileInputStream(pdf);
+			buf = new BufferedInputStream(input);
+			int readBytes = 0;
+
+			// read from the file; write to the ServletOutputStream
+			while ((readBytes = buf.read()) != -1)
+				out.write(readBytes);
+
+		} catch (IOException ioe) {
+			System.out.println("ERROR: " + ioe.getMessage());
+
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e.getMessage());
+
+		} finally {
+
+			if (out != null)
+				out.close();
+
+			if (in != null)
+				in.close();
+
+			if (buf != null)
+				buf.close();
+		}
+	}
+
 	/**
 	 * Отрисовать и вывести отчет
 	 * 
@@ -124,17 +228,13 @@ public final class ReportsMVCController {
 
 				Map<JRExporterParameter, Object> parameters = new HashMap<JRExporterParameter, Object>();
 
-				String root = request.getSession().getServletContext()
-						.getRealPath("/");
+				// String root = request.getSession().getServletContext()
+				// .getRealPath("/");
 
-				String uriPref = "images/report";
+				String uriPref = FileUtils.getCurentTimeDirsPath()
+						+ htmlImageUniqueSuffix;
 
-				if (htmlImageUniqueSuffix != null) {
-					uriPref += FileUtils.getCurentTimeDirsPath()
-							+ htmlImageUniqueSuffix;
-				}
-
-				String undir = root + uriPref;
+				String undir = Config.BASE_FILES_DIR + uriPref;
 
 				FileUtils.mkDirs(undir);
 
@@ -150,10 +250,12 @@ public final class ReportsMVCController {
 
 				parameters
 						.put(net.sf.jasperreports.engine.export.JRHtmlExporterParameter.IMAGES_URI,
-								"/busterminal/" + uriPref + "/");
+								"/busterminal/mvc/report/images" + uriPref
+										+ "/");
 
 				JasperReportsUtils.renderAsHtml(report, parameterMap,
 						JRdataSource, wr, parameters);
+
 			} else if (format.equals("pdf")) {
 				JasperReportsUtils.renderAsPdf(report, parameterMap,
 						JRdataSource, out);
@@ -173,6 +275,24 @@ public final class ReportsMVCController {
 
 			if (out != null)
 				out.close();
+		}
+
+	}
+
+	private ClassPathResource resource = new ClassPathResource(
+			"reports/order.jrxml");
+
+	private JasperReport orderReport;
+
+	{
+
+		try {
+			orderReport = JasperCompileManager.compileReport(resource.getFile()
+					.getAbsolutePath());
+		} catch (JRException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
@@ -211,16 +331,9 @@ public final class ReportsMVCController {
 		parameterMap.put("datasource", JRdataSource);
 
 		// Compile the report
-
-		ClassPathResource resource = new ClassPathResource(
-				"reports/order.jrxml");
-
-		JasperReport report = JasperCompileManager.compileReport(resource
-				.getFile().getAbsolutePath());
-
 		// OUT
-		renderReport(format, "order_" + id, report, parameterMap, JRdataSource,
-				request, response);
+		renderReport(format, "order_" + id, orderReport, parameterMap,
+				JRdataSource, request, response);
 
 	}
 
