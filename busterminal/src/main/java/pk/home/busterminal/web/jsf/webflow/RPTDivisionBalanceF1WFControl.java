8b7a21f6a9fe1a500f0f59200872d79bb9c19308
povloid <povloid@proton2>
2012-06-28 13:34:43 +0600

    работа с отчетными формами

--- busterminal/src/main/java/pk/home/busterminal/web/jsf/webflow/RPTDivisionBalanceF1WFControl.java
+++ busterminal/src/main/java/pk/home/busterminal/web/jsf/webflow/RPTDivisionBalanceF1WFControl.java
@@ -1,19 +1,13 @@
 package pk.home.busterminal.web.jsf.webflow;
 
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
-
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 
 import pk.home.busterminal.domain.Division;
 import pk.home.busterminal.service.DivisionService;
-import pk.home.libs.combine.web.jsf.flow.AWFBasicControl;
+import pk.home.libs.combine.web.jsf.flow.AWFRPTWFControl;
 
-public class RPTDivisionBalanceF1WFControl extends AWFBasicControl implements
+public class RPTDivisionBalanceF1WFControl extends AWFRPTWFControl implements
 		Serializable {
 
 	/**
@@ -21,15 +15,10 @@ public class RPTDivisionBalanceF1WFControl extends AWFBasicControl implements
 	 */
 	private static final long serialVersionUID = -3786051198921606764L;
 
-	private final DateFormat dateFormatFileNameTime = new SimpleDateFormat(
-			"dd-MM-yyyy_HH_mm_ss", new Locale("ru"));
-
 	private Long divisionId;
 	private Division division;
 
 	private Date bDate, eDate;
-
-	private String purl, filename;
 
 	public DivisionService getDivisionService() {
 		return (DivisionService) findBean("divisionService");
@@ -45,39 +34,25 @@ public class RPTDivisionBalanceF1WFControl extends AWFBasicControl implements
 	 * @return
 	 * @throws Exception
 	 */
-	public String makeReport() throws Exception {
-		try {
-			if (division == null)
-				throw new Exception("Не указано подразделение");
+	public MakeResult _makeReport() throws Exception {
 
-			if (bDate == null)
-				throw new Exception("Не указана начальная дата");
+		if (division == null)
+			throw new Exception("Не указано подразделение");
 
-			if (eDate == null)
-				throw new Exception("Не указана конечная дата");
+		if (bDate == null)
+			throw new Exception("Не указана начальная дата");
 
-			if (bDate.compareTo(eDate) > 0)
-				throw new Exception("Начальная дата позже конечной");
+		if (eDate == null)
+			throw new Exception("Не указана конечная дата");
 
-			purl = "/" + division.getId() + "/" + bDate.getTime() + "/"
-					+ eDate.getTime() + "/";
+		if (bDate.compareTo(eDate) > 0)
+			throw new Exception("Начальная дата позже конечной");
 
-			filename = "division_balance_f1_" + division.getId() + "_"
-					+ dateFormatFileNameTime.format(new Date());
+		return new MakeResult(division.getId() + "/" + bDate.getTime()
+				+ "/" + eDate.getTime() 
 
-			return "makeReport";
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			FacesContext.getCurrentInstance().addMessage(
-					null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error: ", e
-							.getMessage()));
-
-			purl = null;
-
-			throw new Exception(e);
-		}
+		, "division_balance_f1_" + division.getId() + "_"
+				+ dateFormatFileNameTime.format(new Date()));
 	}
 
 	// get's and set's
@@ -109,22 +84,6 @@ public class RPTDivisionBalanceF1WFControl extends AWFBasicControl implements
 
 	public Long getDivisionId() {
 		return divisionId;
-	}
-
-	public String getPurl() {
-		return purl;
-	}
-
-	public void setPurl(String purl) {
-		this.purl = purl;
-	}
-
-	public String getFilename() {
-		return filename;
-	}
-
-	public void setFilename(String filename) {
-		this.filename = filename;
 	}
 
 }
